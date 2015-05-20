58707b1dc8c1cc9789af0a52400b299c62c7a02e
banbanchs <memory.silentvoyage@gmail.com>
2015-05-20 09:37:01 +0800

    Fix gravatar-src

--- app/scripts/directives/card.js
+++ app/scripts/directives/card.js
@@ -13,7 +13,7 @@ angular.module('tweetApp')
       + '<div class="card-inner">'
       + '<div class="content">'
       + '<div class="card-header">'
-      + '<a ng-href="#/user/{{ tweet.User.name }}" class="account-group"><img class="avatar img-circle" gravatar-src-once="\'{{ tweet.User.email }}\'" gravatar-size="50" alt="avatar"/><strong class="fullname">{{ tweet.User.name }}</strong><span class="username">{{ tweet.User.name }}</span></a>'
+      + '<a ng-href="#/user/{{ tweet.User.name }}" class="account-group"><img class="avatar img-circle" gravatar-src-once="tweet.User.email" gravatar-size="50" alt="avatar"/><strong class="fullname">{{ tweet.User.name }}</strong><span class="username">{{ tweet.User.name }}</span></a>'
       + '<time class="date pull-right" datetime="{{ tweet.createdAt }}">{{ tweet.createdAt | date:"MM-dd HH:mm"}}</time>'
       + '</div>'
       + '<p class="tweet-content">{{ tweet.content }}</p>'
@@ -25,7 +25,7 @@ angular.module('tweetApp')
         element.next().hide();
         element.click(function() {
           element.parent().toggleClass('open');
-          element.next().slideToggle('300').find('.form-control').removeClass('clicked').next().hide();
+          element.next().slideToggle('300').find('.form-control.clicked').removeClass('clicked').next().hide();
         });
       }
     };
